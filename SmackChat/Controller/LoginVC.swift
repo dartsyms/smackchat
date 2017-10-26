8acf111f0fda908223e720072bc7252d90dbd3dd
Alex Kh <dartsyms@gmail.com>
2017-10-27 01:49:26 +0400

    LoginVC added

---
+++ SmackChat/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  SmackChat
+//
+//  Created by sanchez on 27.10.17.
+//  Copyright © 2017 KOT. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ SmackChat/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SmackChat
+//
+//  Created by sanchez on 27.10.17.
+//  Copyright © 2017 KOT. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
